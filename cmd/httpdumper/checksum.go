9eb99604ae1c6b1bde0fa22d6fae19a3f2369e6a
voidint <voidint@126.com>
2017-08-07 14:05:50 +0800

    Modify feature: add httpdumper settings

--- cmd/httpdumper/checksum.go
+++ cmd/httpdumper/checksum.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+// ChecksumFunc 目标文件摘要校验函数定义。
+// dstFilename 待计算摘要的目标文件。
+// hexHash 目标文件的预期摘要值。
+// 若实际计算的摘要值与预期一致，返回true，否则为false。当摘要计算过程中发生错误，将返回非nil的error值。
+type ChecksumFunc func(dstFilename, hexHash string) (ok bool, err error)
+
 // MD5Checksum 使用MD5摘要算法计算的目标文件的摘要值是否与预期摘要值一致。
 func MD5Checksum(dstFilename, hexHash string) (ok bool, err error) {
 	md5, err := FileMD5(dstFilename)
--- cmd/httpdumper/dumper.go
+++ cmd/httpdumper/dumper.go
@@ -9,11 +9,14 @@ import (
 
 // HTTPDumper HTTP协议文件转储器
 type HTTPDumper struct {
-	url      string
-	method   string
-	header   map[string]string
-	checksum ChecksumFunc
-	hexHash  string
+	url         string
+	method      string
+	header      map[string]string
+	checksum    ChecksumFunc
+	hexHash     string
+	connTimeout time.Duration
+	rwTimeout   time.Duration
+	retries     int
 }
 
 // NewHTTPDumper 实例化http转储器
@@ -25,12 +28,6 @@ func NewHTTPDumper(url, method string, header map[string]string) *HTTPDumper {
 	}
 }
 
-// ChecksumFunc 目标文件摘要校验函数定义。
-// dstFilename 待计算摘要的目标文件。
-// hexHash 目标文件的预期摘要值。
-// 若实际计算的摘要值与预期一致，返回true，否则为false。当摘要计算过程中发生错误，将返回非nil的error值。
-type ChecksumFunc func(dstFilename, hexHash string) (ok bool, err error)
-
 // Checksum 设置checksum的函数实现
 func (d *HTTPDumper) Checksum(hexHash string, checksum ChecksumFunc) *HTTPDumper {
 	d.hexHash = hexHash
@@ -38,8 +35,21 @@ func (d *HTTPDumper) Checksum(hexHash string, checksum ChecksumFunc) *HTTPDumper
 	return d
 }
 
+// Timeout 设置连接超时时间和读写超时时间
+func (d *HTTPDumper) Timeout(connTimeout, readWriteTimeout time.Duration) *HTTPDumper {
+	d.connTimeout = connTimeout
+	d.rwTimeout = readWriteTimeout
+	return d
+}
+
+// Retries 设置重试次数
+func (d *HTTPDumper) Retries(retries int) *HTTPDumper {
+	d.retries = retries
+	return d
+}
+
 var (
-	// ErrChecksum 无效的checksum
+	// ErrChecksum 无效的checksum错误
 	ErrChecksum = errors.New("invalid checksum")
 )
 
@@ -49,7 +59,12 @@ func (d *HTTPDumper) Dump(dstFilename string) (err error) {
 	for k, v := range d.header {
 		req.Header(k, v)
 	}
-	if err = req.SetTimeout(time.Second, time.Second).Retries(3).ToFile(dstFilename); err != nil {
+
+	if d.connTimeout > 0 && d.rwTimeout > 0 {
+		req.SetTimeout(d.connTimeout, d.rwTimeout)
+	}
+
+	if err = req.Retries(d.retries).ToFile(dstFilename); err != nil {
 		return err
 	}
 
